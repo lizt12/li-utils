cfa1df96e2a39cc676167185786f77b1a37cc228
li_zt <1633126365@qq.com>
2024-12-26 08:22:17 +0800

    build: 优化项目构建配置并添加新功能
    - 更新 .babelrc 文件，启用 Babel 转换
    - 修改 package.json，将主目录从 dist 改为 src
    - 重构 webpack.config.js，增加多入口文件支持
    - 新增 tree.js 和 operation.js 模块
    - 配置 babel-loader 以转换 ES6 代码

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,34 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url)); // 模拟 CommonJS 的 __dirname
 
 export default {
-  entry: "./src/index.js", // 入口文件
+  // 入口文件
+  entry: {
+    index: "./src/index.js",
+    tree: './src/tree.js',
+    operation: './src/operation.js',
+  },
   output: {
-    path: path.resolve(__dirname, "dist"), // 输出目录
-    filename: "index.js", // 输出文件名
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js', // 使用[name]来确保每个文件名不同
     library: {
       name: "li-zt-utils", // 替换为你的库名称
       type: "umd", // 支持多种模块化规范
     },
+  },
+  // 模块规则，例如使用 Babel 转换 ES6 代码
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // 适用于所有 .js 文件
+        exclude: /node_modules/, // 排除 node_modules
+        use: {
+          loader: "babel-loader", // 使用 Babel loader
+          options: {
+            presets: ["@babel/preset-env"], // Babel 配置
+          },
+        },
+      },
+    ],
   },
   mode: "production", // 使用生产模式，优化代码
   optimization: {
